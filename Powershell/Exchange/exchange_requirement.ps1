5d0a07e0913bacfec39d86b021a0fa5e30600cce
Launay <launay.ronan@gmail.com>
2014-08-28 11:59:39 +0200

    exchange_requirement.ps1
    exchange_requirement.ps1

---
+++ Powershell/Exchange/exchange_requirement.ps1
@@ -0,0 +1,3 @@
+Import-Module ServerManager
+Add-WindowsFeature NET-Framework,RSAT-ADDS,Web-Server,Web-Basic-Auth,Web-Windows-Auth,Web-Metabase,Web-Net-Ext,Web-Lgcy-Mgmt-Console,WAS-Process-Model,RSAT-Web-Server,Web-ISAPI-Ext,Web-Digest-Auth,Web-Dyn-Compression,NET-HTTP-Activation,RPC-Over-HTTP-Proxy -Restart
+Set-Service NetTcpPortSharing -StartupType Automatic
