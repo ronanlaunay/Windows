f3d8b1bc186de124c68f48e02570adb9279cdff7
Launay <launay.ronan@gmail.com>
2015-01-15 16:07:40 +0100

    Update GetADUser_name-SAN.ps1

--- Powershell/GetADUser_name-SAN.ps1
+++ Powershell/GetADUser_name-SAN.ps1
@@ -1,2 +1,2 @@
 Import-Module ActiveDirectory
-Get-ADUser -searchbase "OU=Service Role Accounts,OU=WolseleyFrance,OU=WOS Operating Companies,DC=DS,DC=WOLSELEY,DC=COM" -Filter * | sort $_.name | FT name,samaccountname
+Get-ADUser -searchbase "OU=Service Role Accounts,OU=Company,OU=All Company,DC=DS,DC=COMPANY,DC=COM" -Filter * | sort $_.name | FT name,samaccountname
