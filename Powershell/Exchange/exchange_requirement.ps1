be1976c79eed27de1132eecd3675b82e6edcb332
Ronan Launay <ronan@alkivi.fr>
2016-07-06 11:52:52 +0200

    Update exchange_requirement.ps1

--- Powershell/Exchange/exchange_requirement.ps1
+++ Powershell/Exchange/exchange_requirement.ps1
@@ -1,3 +1,4 @@
 Import-Module ServerManager
-Add-WindowsFeature NET-Framework,RSAT-ADDS,Web-Server,Web-Basic-Auth,Web-Windows-Auth,Web-Metabase,Web-Net-Ext,Web-Lgcy-Mgmt-Console,WAS-Process-Model,RSAT-Web-Server,Web-ISAPI-Ext,Web-Digest-Auth,Web-Dyn-Compression,NET-HTTP-Activation,RPC-Over-HTTP-Proxy -Restart
+Install-WindowsFeature RSAT-ADDS
+Install-WindowsFeature AS-HTTP-Activation, Desktop-Experience, NET-Framework-45-Features, RPC-over-HTTP-proxy, RSAT-Clustering, RSAT-Clustering-CmdInterface, Web-Mgmt-Console, WAS-Process-Model, Web-Asp-Net45, Web-Basic-Auth, Web-Client-Auth, Web-Digest-Auth, Web-Dir-Browsing, Web-Dyn-Compression, Web-Http-Errors, Web-Http-Logging, Web-Http-Redirect, Web-Http-Tracing, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Lgcy-Mgmt-Console, Web-Metabase, Web-Mgmt-Console, Web-Mgmt-Service, Web-Net-Ext45, Web-Request-Monitor, Web-Server, Web-Stat-Compression, Web-Static-Content, Web-Windows-Auth, Web-WMI, Windows-Identity-Foundation, NET-Framework, RSAT-Web-Server, NET-HTTP-Activation, -Restart
 Set-Service NetTcpPortSharing -StartupType Automatic
